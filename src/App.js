93b6ff10256c2a1a51c6ce54d68006c1bf861e4b
AlexvonKarstedt <80624199+AlexvonKarstedt@users.noreply.github.com>
2021-05-08 17:51:18 -0400

    Added Bootstrap npm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className ="Container">   
       <header>
         Weather App
       </header>
       <footer>
     Open-Source code by <a href ="https://github.com/AlexvonKarstedt/react-weather-project" target="_">Alexandra von Karstedt</a> 
     </footer>
+    </div>
     </div>
   );
 }

7f1e43cfa91fd29c77ebe029915012e296636737
AlexvonKarstedt <80624199+AlexvonKarstedt@users.noreply.github.com>
2021-05-12 18:58:43 -0400

    added CSS to Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,28 @@ import React from 'react'
 import './Weather.css'
 export default function Weather (){
     return (
-        <div className ="Weather">Hello from Weather!</div>
+        <div className ="Weather">       
+     <h1>Barrie, ON, Canada</h1>
+     <ul>
+     <li>Monday 6:00 PM</li>
+     <li>Sunny</li>
+     </ul>     
+       <div className="row"> 
+       <div className="col-6">
+         <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny"/>
+         26℃ | ℉
+         </div>
+       <div className="col-6">
+         <ul>
+           <li>Precipitation: 33%</li>
+           <li>Humidity: 79%</li>
+           <li>Wind: 21 km/h</li>
+           </ul>
+           </div>
+       
+        
+       </div>
+      </div>
+      
     );
 }

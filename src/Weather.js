7a19266b65a5e271e5d0b94401795e4073355c1c
AlexvonKarstedt <80624199+AlexvonKarstedt@users.noreply.github.com>
2021-05-13 20:42:33 -0400

    added useState to Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios';
 import './Weather.css'
 export default function Weather (){
+  const [temperature, setTemperature] =useState(null);
   function showTemperature(response){
     console.log(response.data);
   let city ="Barrie"
